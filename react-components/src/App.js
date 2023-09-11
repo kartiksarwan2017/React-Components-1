28736590b2b155efb85f49cc9572faf5d55674f9
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-09-11 13:42:26 +0530

    Class Based Components, NFTium asssignment

---
+++ react-components/src/App.js
@@ -0,0 +1,21 @@
+// import React from "react";
+
+// class App extends React.Component {
+//   render(){
+//     return <h1>Class Component</h1>
+//   }
+// }
+
+// export default App;
+
+
+import React from "react";
+
+export default class App extends React.Component {
+
+  render(){
+    return <h1>Class Component</h1>
+  }
+
+}
+
